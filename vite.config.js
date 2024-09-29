326df9ae78512df4b955ebc4d874f1a2fc89376a
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-09-30 00:19:59 +0700

    ds

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 1001,
+    port: 1001,
     host: true,
   },
 })
